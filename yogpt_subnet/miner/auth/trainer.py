8bcb0c38f2fc6aeffe013e7444133e0d97868665
Tobius <tobiusaolo21@gmail.com>
2024-07-14 13:27:25 +0300

    trainer added

--- yogpt_subnet/miner/auth/trainer.py
+++ yogpt_subnet/miner/auth/trainer.py
@@ -1,161 +1,219 @@
+import argparse
 import asyncio
+import json
 import os
 import signal
+import ssl
 import sys
-import json
-from typing import Optional
+import time
 
-import uvicorn
+import aiohttp
+import pyfiglet
+from communex.module.module import Module, endpoint
 from dotenv import load_dotenv
 from rich.console import Console
-from rich.prompt import Prompt
+from rich.live import Live
 from rich.panel import Panel
+from rich.progress import Progress, SpinnerColumn, TextColumn
+from rich.table import Table
 from rich.text import Text
-import pyfiglet
 
-from communex.module.module import Module
-from yogpt_subnet.miner.auth.auth import authenticate
-from yogpt_subnet.miner.finetune.runpod.pipeline import generate_pipeline_script
-from yogpt_subnet.miner.utils.helpers import (
-    fetch_jobs, fetch_and_save_job_details, update_job_status,
-    register_completed_job, submit_to_runpod
-)
-from yogpt_subnet.miner.finetune.llama_fine_tune import fine_tune_llama
-from yogpt_subnet.miner.finetune.gpt_fine_tune import fine_tune_gpt
-from yogpt_subnet.miner.finetune.open_elm import fine_tune_openELM
-class Config:
-    def __init__(self):
-        load_dotenv()
-        self.base_url = os.getenv("BASE_URL")
-        self.hf_access_token = os.getenv("HF_ACCESS_TOKEN")
-        self.token = os.getenv("TOKEN")
-        self.miner_id = os.getenv("MINER_ID")
-        self.run_on_runpod: bool = os.getenv('RUN_ON_RUNPOD', 'false').lower() == 'true'
-        self.runpod_api_key: Optional[str] = os.getenv('RUNPOD_API_KEY')
+from yogpt_subnet.miner.finetune.runpod.pipeline import \
+    generate_pipeline_script
 
-        if not self.base_url or not self.hf_access_token:
-            raise ValueError("BASE_URL and HF_ACCESS_TOKEN must be set in the environment or .env file")
+# Append directories to sys.path for relative imports
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../', 'finetune')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'utils')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './', 'auth')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'finetune/runpod')))
+from yogpt_subnet.miner.auth.auth import authenticate  # type:ignore
+from yogpt_subnet.miner.finetune.gpt_fine_tune import \
+    fine_tune_gpt  # type:ignore
+from yogpt_subnet.miner.finetune.llama_fine_tune import \
+    fine_tune_llama  # type:ignore
+from yogpt_subnet.miner.finetune.open_elm import \
+    fine_tune_openELM  # type:ignore
+from yogpt_subnet.miner.utils.helpers import (fetch_and_save_job_details,
+                                              fetch_jobs,
+                                              register_completed_job,
+                                              submit_to_runpod,
+                                              update_job_status)
 
-        if self.run_on_runpod and not self.runpod_api_key:
-            raise ValueError("RUNPOD_API_KEY must be set if RUN_ON_RUNPOD is true")
 
 class Trainer(Module):
     def __init__(self):
-        super().__init__()
+        load_dotenv()
+        self.BASE_URL = os.getenv("BASE_URL")
+        self.HF_ACCESS_TOKEN = os.getenv("HF_ACCESS_TOKEN")
+        self.TOKEN = os.getenv("TOKEN")
+        self.MINER_ID = os.getenv("MINER_ID")
         self.console = Console()
-        self.config = Config()
-        self.current_job_id: Optional[str] = None
+        self.current_job_id = None  # Global variable to store the current job ID
 
+    @endpoint
     def display_welcome_message(self):
         fig = pyfiglet.Figlet(font='slant')
         welcome_text = fig.renderText('YOGPT Miner')
         self.console.print(welcome_text, style="bold blue")
         self.console.print(Panel(Text("Welcome to YOGPT Miner System!", justify="center"), style="bold green"))
 
-    def get_user_credentials(self):
-        username = Prompt.ask("Enter your username")
-        password = Prompt.ask("Enter your password", password=True)
-        return username, password
+    @endpoint
+    async def fetch_jobs(self):
+        timeout = aiohttp.ClientTimeout(total=120)  # Increased timeout to 120 seconds
+        ssl_context = ssl.create_default_context()
+        ssl_context.check_hostname = False
+        ssl_context.verify_mode = ssl.CERT_NONE
 
-    def authenticate_user(self, username: str, password: str) -> bool:
-        token, miner_id = authenticate(username, password)
-        if not token or not miner_id:
-            self.console.print("[bold red]Authentication failed.[/bold red]")
-            return False
-        self.config.token, self.config.miner_id = token, miner_id
-        return True
-
-    async def process_job(self, job_details: dict):
+        async with aiohttp.ClientSession(timeout=timeout, connector=aiohttp.TCPConnector(ssl=ssl_context)) as session:
+            headers = {'Authorization': f'Bearer {self.TOKEN}'}
+            try:
+                async with session.get(f"{self.BASE_URL}/pending-jobs", headers=headers) as response:
+                    if response.status == 200:
+                        return await response.json()
+                    elif response.status == 401:
+                        return "Unauthorized"
+                    else:
+                        self.console.log(f"Failed to fetch jobs: {await response.text()}")
+                        return []
+            except aiohttp.ClientConnectorError as e:
+                self.console.log(f"Connection error: {e}")
+                return []
+            
+    async def fetch_and_save_job_details(self, job_id):
+        async with aiohttp.ClientSession() as session:
+            headers = {'Authorization': f'Bearer {self.TOKEN}'}
+            async with session.post(f"{self.BASE_URL}/start-training/{job_id}", headers=headers, json={'minerId': self.MINER_ID}) as response:
+                if response.status == 200:
+                    job_details = await response.json()
+                    job_dir = os.path.join(os.getcwd(), 'jobs', job_id)
+                    os.makedirs(job_dir, exist_ok=True)
+                    details_path = os.path.join(job_dir, 'details.json')
+                    job_details['jobId'] = job_id
+                    with open(details_path, 'w') as f:
+                        json.dump(job_details, f, indent=2)
+                    return details_path
+                else:
+                    self.console.log(f"Failed to start training for job {job_id}: {await response.text()}")
+                    return None
+ 
+    async def update_job_status(self, job_id, status):
+        url = f"{self.BASE_URL}/update-status/{job_id}"
+        async with aiohttp.ClientSession() as session:
+            headers = {'Authorization': f'Bearer {self.TOKEN}', 'Content-Type': 'application/json'}
+            async with session.patch(url, json={'status': status}, headers=headers) as response:
+                try:
+                    if response.status == 200:
+                        self.console.log(f"Status updated to {status} for job {job_id}")
+                    else:
+                        response.raise_for_status()
+                except aiohttp.ClientResponseError as err:
+                    self.console.log(f"Failed to update status for job {job_id}: {err}")
+                except Exception as e:
+                    self.console.log(f"An error occurred: {e}")
+  
+    async def process_job(self, job_details, run_on_runpod=False, runpod_api_key=None):
+        global current_job_id
         model_id = job_details['baseModel']
         dataset_id = job_details['huggingFaceId']
         job_id = job_details['jobId']
         new_model_name = job_id
-        self.current_job_id = job_id
+        self.current_job_id = job_id  # Update the current job ID
 
         try:
-            if self.config.run_on_runpod:
+            if run_on_runpod:
+                if runpod_api_key is None:
+                    raise ValueError("RunPod API key is required when running on RunPod.")
                 script_path = f"pipeline_script_{job_id}.py"
                 generate_pipeline_script(job_details, script_path)
-                submit_to_runpod(script_path, self.config.runpod_api_key)
+                submit_to_runpod(script_path, runpod_api_key)
             else:
                 model_repo_url, loss, accuracy, total_pipeline_time = None, None, None, None
+                self.console.log(f"General model {model_id}")
                 model_detected = model_id.lower()
 
                 if 'llama' in model_detected:
-                    model_repo_url, loss, accuracy, total_pipeline_time = await fine_tune_llama(
-                        model_id, dataset_id, new_model_name, self.config.hf_access_token, job_id
-                    )
+                    self.console.log(f"Model is of type Llama: {model_id}")
+                    model_repo_url, loss, accuracy, total_pipeline_time = await fine_tune_llama(model_id, dataset_id, new_model_name, self.HF_ACCESS_TOKEN, job_id)
                 elif 'gpt' in model_detected:
-                    model_repo_url, loss, accuracy, total_pipeline_time = await fine_tune_gpt(
-                        model_id, dataset_id, new_model_name, self.config.hf_access_token, job_id
-                    )
+                    self.console.log(f"Model is of type GPT: {model_id}")
+                    model_repo_url, loss, accuracy, total_pipeline_time = await fine_tune_gpt(model_id, dataset_id, new_model_name, self.HF_ACCESS_TOKEN, job_id)
                 elif 'openelm' in model_detected:
-                    model_repo_url, loss, accuracy, total_pipeline_time = await fine_tune_openELM(
-                        model_id, dataset_id, new_model_name, self.config.hf_access_token, job_id
-                    )
+                    self.console.log(f"Model is of type OpenELM: {model_id}")
+                    model_repo_url, loss, accuracy, total_pipeline_time = await fine_tune_openELM(model_id, dataset_id, new_model_name, self.HF_ACCESS_TOKEN, job_id)
                 else:
-                    raise ValueError(f"Unsupported model ID: {model_id}")
+                    self.console.log(f"Unsupported model ID: {model_id}. Skipping job.")
+                    return  # Skip this job and proceed to the next one
 
-                if model_repo_url:
+                if model_repo_url is not None:
                     self.console.log(f"Model uploaded to: {model_repo_url}")
                     self.console.log(f"Total pipeline time: {total_pipeline_time}")
-                    await update_job_status(job_id, 'completed', self.config.base_url, self.config.token)
-                    await register_completed_job(job_id, model_repo_url, loss, accuracy, total_pipeline_time,
-                                                 self.config.base_url, self.config.token)
+
+                    await self.update_job_status(job_id, 'completed')
+                    await register_completed_job(job_id, model_repo_url, loss, accuracy, total_pipeline_time)
                 else:
-                    raise RuntimeError(f"Failed to process job {job_id}: No model URL returned")
+                    self.console.log(f"Failed to process job {job_id}: No model URL returned")
+                    await self.update_job_status(job_id, 'failed')
 
+        except RuntimeError as e:
+            self.console.log(f"Failed to process job {job_id}: {str(e)}")
         except Exception as e:
-            self.console.log(f"Error processing job {job_id}: {str(e)}")
-            await update_job_status(job_id, 'failed', self.config.base_url, self.config.token)
+            await self.update_job_status(job_id, 'pending')
+            self.console.log(f"Unexpected error occurred while processing job {job_id}: {str(e)}")
         finally:
-            self.current_job_id = None
-
-    async def main_loop(self):
-        while True:
-            try:
-                jobs = await fetch_jobs(self.config.base_url, self.config.token)
-                if jobs:
-                    job_id = jobs[0]['id']
-                    self.console.log(f"Executing JobId: {job_id}")
-                    job_details_path = await fetch_and_save_job_details(job_id, self.config.base_url, 
-                                                                        self.config.token, self.config.miner_id)
-                    if job_details_path:
-                        with open(job_details_path, 'r') as file:
-                            job_details = json.load(file)
-                        await self.process_job(job_details)
-                await asyncio.sleep(2)
-            except Exception as e:
-                self.console.log(f"Error in main loop: {str(e)}")
-                await asyncio.sleep(5)
-
+            self.current_job_id = None  # Reset the current job ID
+ 
     def handle_interrupt(self, signal, frame):
         if self.current_job_id:
-            asyncio.run(update_job_status(self.current_job_id, 'pending', 
-                                          self.config.base_url, self.config.token))
+            asyncio.run(self.update_job_status(self.current_job_id, 'pending'))
         self.console.log("[bold red]Interrupted. Exiting...[/bold red]")
         sys.exit(0)
 
     async def main(self):
         self.display_welcome_message()
 
-        while True:
-            username, password = self.get_user_credentials()
-            if self.authenticate_user(username, password):
-                self.console.print("[bold green]Authentication successful. Starting server...[/bold green]")
-                break
-            retry = Prompt.ask("Authentication failed. Do you want to try again?", choices=["yes", "no"], default="yes")
-            if retry.lower() != "yes":
-                self.console.print("Exiting program.")
-                return
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
 
-        signal.signal(signal.SIGINT, self.handle_interrupt)
-        
-        # Start your Uvicorn server here if needed
-        # uvicorn.run(app, host="0.0.0.0", port=8889)
+        token, miner_id = authenticate(username, password)
+        if not token or not miner_id:
+            self.console.log("[bold red]Authentication failed. Exiting...[/bold red]")
+            sys.exit(1)
 
-        await self.main_loop()
+        self.TOKEN, self.MINER_ID = token, miner_id
+
+        progress_table = Table.grid(expand=True)
+        progress_table.add_column(justify="center", ratio=1)
+        status_panel = Panel("Waiting for training jobs...", title="Status", border_style="green")
+        progress_table.add_row(status_panel)
+
+        with Live(progress_table, refresh_per_second=10, console=self.console) as live:
+            while True:
+                live.update(progress_table)
+                jobs = await self.fetch_jobs()
+
+                if jobs == "Unauthorized":
+                    self.console.log("[bold red]Unauthorized access. Please check your credentials.[/bold red]")
+                    break
+                elif jobs:
+                    job_id = jobs[0]['id']
+                    self.console.log(f"Executing JobId: {job_id}")
+                    job_details_path = await self.fetch_and_save_job_details(job_id)
+                    if job_details_path:
+                        self.console.log(f"[green]Job {job_id} fetched successfully[/green]")
+                        status_panel = Panel(f"Processing JobId: {job_id}", title="Status", border_style="yellow")
+                        live.update(progress_table)
+
+                        with open(job_details_path, 'r') as file:
+                            job_details = json.load(file)
+                        await self.process_job(job_details)
+
+                        status_panel = Panel("Waiting for training jobs...", title="Status", border_style="green")
+                        live.update(progress_table)
+                        self.console.log(f"[green]Job {job_id} processed successfully[/green]")
+                await asyncio.sleep(2)
 
 if __name__ == "__main__":
     trainer = Trainer()
-    asyncio.run(trainer.run())
+    signal.signal(signal.SIGINT, trainer.handle_interrupt)
+    asyncio.run(trainer.main())
+
