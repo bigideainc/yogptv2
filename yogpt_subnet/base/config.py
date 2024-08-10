e3f2d4de24d7f3546ec5852ff0d289050f38c18d
Tobius <tobiusaolo21@gmail.com>
2024-08-10 15:40:00 +0300

    yogptread

--- yogpt_subnet/base/config.py
+++ yogpt_subnet/base/config.py
@@ -5,12 +5,3 @@ from typing import List
 class YogptBaseSettings(BaseSettings):
     use_testnet: bool = False
     call_timeout: int = 60
-
-    # TODO: whitelist&blacklist
-    # whitelist: List[str] = []
-    # blacklist: List[str] = []
-
-    # class Config:
-    #     env_prefix = "MOSAIC"
-    #     env_file = "env/config.env"
-    #     extra = "ignore"
